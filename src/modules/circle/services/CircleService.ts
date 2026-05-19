782c42fe8818c9c6f19b84abf7bfe7702936f9ff
Muhammad Rafadana Mountheira <rafadanam@gmail.com>
2026-05-19 20:43:55 +0700

    feat: remove fandom data

--- src/modules/circle/services/CircleService.ts
+++ src/modules/circle/services/CircleService.ts
@@ -10,6 +10,9 @@ function createCircleService(): CircleService {
       const file = Bun.file('./live_data/circles.json');
       const circleData = (await file.json()) as NormalizedCircles;
 
+      // temporarily disable fandoms as it is not used and there are dozens of them
+      circleData.fandoms = [];
+
       return circleData;
     }
   };
